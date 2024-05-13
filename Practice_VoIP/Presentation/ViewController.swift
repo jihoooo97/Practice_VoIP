b3183d944b3187afe305f8910133fb028a8cde29
Jiho <yjh7827@naver.com>
2024-05-13 18:43:15 +0900

    feat: 기본 UI 구성

--- Practice_VoIP/Presentation/ViewController.swift
+++ Practice_VoIP/Presentation/ViewController.swift
@@ -9,11 +9,56 @@ import UIKit
 
 final class ViewController: BaseViewController {
 
+    private lazy var receivingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("전화받기", for: .normal)
+        button.configuration = .borderedProminent()
+        button.addTarget(self, action: #selector(didTapReceivingButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private lazy var callingButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("전화걸기", for: .normal)
+        button.configuration = .borderedProminent()
+        button.addTarget(self, action: #selector(didTapCallingButton), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initLayout()
     }
 
+    
+    private func initLayout() {
+        view.addSubview(receivingButton)
+        view.addSubview(callingButton)
+        
+        NSLayoutConstraint.activate([
+            receivingButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            receivingButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -40)
+        ])
+        
+        NSLayoutConstraint.activate([
+            callingButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            callingButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 40)
+        ])
+    }
+    
+    @objc
+    private func didTapReceivingButton() {
+        print("receiving action")
+    }
+    
+    @objc
+    private func didTapCallingButton() {
+        print("calling action")
+    }
 
 }
 
